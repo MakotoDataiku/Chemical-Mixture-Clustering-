f4ce2887c477db3d34d93b6b45427ee5bb00d7ee
admin <makoto.miyazaki@dataiku.com>
2020-10-30 11:26:31 +0900

    Recipe KANEKACORPDEMO.compute_readings_new saved through notebook notebook editor for compute_readings_new

--- recipes/compute_readings_new.py
+++ recipes/compute_readings_new.py
@@ -14,6 +14,25 @@ l = df.index.values
 l = l * 0.01
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+for i in range(0, len(l)):
+    if i < 120:
+        l[i] = l[i]
+    elif i < 150:
+        l[i] = l[i] * 1.2
+    elif i < 180:
+        l[i] = l[i] * 1.4
+    elif i < 200:
+        l[i] = l[i] * 1.6
+    elif i < 220:
+        l[i] = l[i] * 1.8
+    elif i < 240:
+        l[i] = l[i] * 2.0
+    elif i < 300:
+        l[i] = l[i] * 2.2
+    elif i < 400:
+        l[i] = l[i] * 2.3
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for c in df.columns.drop("Hz"):
     df[c] = df[c] * l
 
